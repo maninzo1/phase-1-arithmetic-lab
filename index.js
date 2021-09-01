650de97fefc8be367926f0685d70d764aaed84e5
MaryAninzo <aninzom211@gmail.com>
2021-08-31 21:32:05 -0400

    commit tree 06f48d1151a3566363fa960a0c2d4221d883c6cc

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
+let num1 = 2
+let num2 = 31
+let multiply = num1 * num2
 
-// Write your code here
+console.log(multiply)
+
+let random = Math.ceil(Math.random());
+
+let num3 = 20
+let num4 = 5
+let mod = num3/num4
+
+let max = Math.max(20); 
